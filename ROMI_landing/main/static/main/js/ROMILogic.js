9659995a5d117cffc29bc3e9e20d8ff0f47ac300
DoarBell <vidalkijuro@gmail.com>
2026-09-13 21:32:01 -0600

    Did something and I am exausted, I was like 2 hours looking for making a video for ROMI but the reality is that ai for videos is expensive and I am broke af

--- ROMI_landing/main/static/main/js/ROMILogic.js
+++ ROMI_landing/main/static/main/js/ROMILogic.js
@@ -2,4 +2,9 @@ document.addEventListener('DOMContentLoaded', () => {
     function toggleTheme(){
         document.body.classList.toggle("dark-theme");
     }
+
+    const themeBtn = document.querySelector("#theme-btn")
+    themeBtn.addEventListener("click", () => {
+        toggleTheme();
+    })
 })
